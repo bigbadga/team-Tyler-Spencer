4105b979f976b86be206ffb0a960015e5bf4cfbb
che11_000 <che11_000@10.18.149.20>
2016-06-17 14:24:42 -0600

    6/16/2016-moveView and moveSpaceView

--- Mansion/src/byui/cit260/mansion/view/MoveView.java
+++ Mansion/src/byui/cit260/mansion/view/MoveView.java
@@ -9,59 +9,61 @@ package byui.cit260.mansion.view;
  *
  * @author che11_000
  */
-public class MoveView extends View{
+public class MoveView extends View {
 
-    
-            
+    public MoveView() {
+        super("Where do you want to go? "
+                + "\nPlease enter a direction you want to move. (ei, west=W) "
+        );
+    }
 
-       public MoveView(){
-           super("Where do you want to go? "
-                +"\nPlease enter a direction you want to move. (ei, west=W) "
-           
-           );
-       }
-    
-    
     @Override
     public boolean doAction(String value) {
-        
-      
-        
-    
-    value=value.toUpperCase();
-     switch (value) {
+
+        value = value.toUpperCase();
+        switch (value) {
             case "W":
-               
+
                 this.moveSpace();
                 break;
-               case "E":
-                
+
+            case "E":
+
                 this.moveSpace();
                 break;
-               case "N":
-                
+
+            case "N":
+
                 this.moveSpace();
                 break;
-               case "S":
-                
+
+            case "S":
+
                 this.moveSpace();
                 break;
-               default:
+
+            default:
                 System.out.println("\n*** Invalid selection *** Try again");
                 break;
-     }
-     return false;
+        }
+        return false;
     }
-     
-     private void moveSpace() {
-     MoveSpaceView movespace = new MoveSpaceView();
-        movespace.display();
-    
+
+    private void moveSpace() {
+        displayMessage = "How many spaces do you want to move \"\n"
+                + "\nPlease enter a number between 1 to 10.";
+
+        String num2 = this.getInput();
+        int num3 = Integer.parseInt(num2);
+        if (num3 > 0 && num3<11) {
+            displayMessage="\n*** You are at a new position***"
+                    +"\nEnter another direction if you want to move again."
+                    +"\nOr enter Q to exit.";
+        } 
+        else {
+            System.out.println("\n*** Invalid Number *** Try again(1-10)");
+            this.moveSpace();
+        }
     }
-    
-    
-    
-    
-   
-    
+
 }
