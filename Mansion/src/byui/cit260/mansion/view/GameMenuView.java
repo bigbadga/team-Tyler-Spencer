73f2006f47c4edb632ce4fab29e9813ba0c2909c
tylerbadger <tylerbadger@10.18.150.115>
2016-07-12 13:07:11 -0600

    7/12/16 Tried to do the Lesson 11 assignment

--- Mansion/src/byui/cit260/mansion/view/GameMenuView.java
+++ Mansion/src/byui/cit260/mansion/view/GameMenuView.java
@@ -3,6 +3,11 @@ package byui.cit260.mansion.view;
 import byui.cit260.mansion.model.Location;
 import byui.cit260.mansion.model.Scene;
 import byui.cit260.mansion.model.Map;
+import byui.cit260.mansion.model.Scene.SceneType;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 /**
  *
@@ -12,6 +17,8 @@ public class GameMenuView extends View {
     
     private String gameMenu;
     private Location[][] map = Map.getLocations();
+    private String outputLocation;
+    private Scene SceneType;
     
     public GameMenuView(){
         super("\n"
@@ -29,6 +36,7 @@ public class GameMenuView extends View {
               + "\nG - Guess the murderer"
               + "\nW - Find Weapon"
               + "\nF - Find Scene"
+              + "\nP - Print File"
               + "\nQ - Quit"
               + "\n------------------------------------------------------------");
     }
@@ -70,6 +78,9 @@ public class GameMenuView extends View {
                 break;
             case "F":
                 this.findScene();
+                break;
+            case "P":
+                this.printScenes(SceneType, outputLocation);
                 break;
             default:
                 System.out.println("\n*** Invalid selection *** Try again");
@@ -147,7 +158,25 @@ public class GameMenuView extends View {
         FindSceneView findScene = new FindSceneView();
         findScene.display();
     }
+    
+    public void printScenes(enum SceneType, String outputLocation) {
+        
+        // create BufferedReader object for input file
+        try (PrintWriter out = new PrintWriter(outputLocation)) {
+            
+            // print title and column headings
+            out.println("\n\n               Scene Report               ");
+            out.printf("%n%-20s%10s%10s", "Description", "Map Symbol", "Blocked?");
+            out.printf("%n%-20s%10s%10s", "-----------", "----------", "--------");
+            
+            for(SceneType scene : SceneType) {
+                out.printf("%n%-20s%7d%13.2f", scene.getDescription()
+                                             , scene.getMapSymbol()
+                                             , scene.getBlockedRoom());
+            }
+            
+        } catch (IOException ex) {
+            System.out.println("I/O Error: " + ex.getMessage());
+        } 
+    }
 }
-
-
-
