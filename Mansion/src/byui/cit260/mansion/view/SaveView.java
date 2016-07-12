93c891bf8cbe72fa0d47e1c1e8a2cd72b366f1a8
che11_000 <che11_000@10.18.149.20>
2016-07-12 14:19:34 -0600

    7-12-2016 : savetofile

--- Mansion/src/byui/cit260/mansion/view/SaveView.java
+++ Mansion/src/byui/cit260/mansion/view/SaveView.java
@@ -8,7 +8,12 @@ package byui.cit260.mansion.view;
 import byui.cit260.mansion.model.Item.BackpackItem;
 
 import byui.cit260.mansion.model.Item;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import static java.lang.System.out;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,15 +22,16 @@ import static java.lang.System.out;
 public class SaveView extends View {
 
     public SaveView() {
-        super("Are you sure you want to save the game? Y=YES, Q= NO and Quit"
+        super("Where do you want to save the game at? or enter Q to quit."
         );
     }
 
     @Override
     public boolean doAction(String value) {
-        value = value.toUpperCase();
+        PrintWriter outfile = null;
+        try {
 
-        if (value.equals("Y")) {
+            outfile = new PrintWriter(value);
             out.println("\n-----------------------Backpack items-----------------------");
             out.printf("%n%-20s%5s%20s", "ItemName", "IndexNumber", "Description");
             out.printf("%n%-20s%5s%20s", "--------", "-----------", "-----------");
@@ -35,14 +41,21 @@ public class SaveView extends View {
                 out.printf("%n%-20s%5d%28s", item.name(), item.ordinal(), item.getDescription());
                 //out.println(item.name()+", "+item.ordinal());
             }
+            outfile.flush();
+        } catch (IOException ex) {
+            out.println("\nError in saving process.");
+        } finally {
+            if (outfile != null) {
+                outfile.close();
+                out.println("\nYOur game has been saved.");
 
+            }
         }
 
-        return false;
+        // private void showItem() {
+        //     ShowItemView showItem = new ShowItemView();
+        //     showItem.display(); //To change body of generated methods, choose Tools | Templates.
+        // }
+        return true;
     }
-
-    // private void showItem() {
-    //     ShowItemView showItem = new ShowItemView();
-    //     showItem.display(); //To change body of generated methods, choose Tools | Templates.
-    // }
 }
